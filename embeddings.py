ea80863df6540b68313484b1af0c94dd06f445c7
Mohamed-said-abdalaziz12 <mohamedshahin2050softwere@gmail.com>
2026-09-23 05:39:45 +0300

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,9 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-
-def get_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True}
-    )
